a265950c1e7c64cec988b2c4a6e7882178bcafe1
xfekke <fekke4201337@gmail.com>
2024-10-21 15:08:30 +0200

    Update

---
+++ tests/config.js
@@ -0,0 +1,11 @@
+// right now chrome is our only options since
+// we haven't installed other drivers
+// and have special settings in support/world.js
+export const browser = 'chrome';
+// run headless (don't show browser)
+export const headless = false;
+// max execution time
+export const timeout = 30000;
+// to run in 'slow motion' increase this time
+// and to run at maximal speed keep at 0
+export const sleepBetweenSteps = 0;
