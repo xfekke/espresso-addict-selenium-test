15e601a123b8af536d246ec3ef3fce080047fab5
AlbinBjerhem <143437786+AlbinBjerhem@users.noreply.github.com>
2024-10-25 11:13:06 +0200

    Test

--- tests/config.js
+++ tests/config.js
@@ -5,7 +5,7 @@ export const browser = 'chrome';
 // run headless (don't show browser)
 export const headless = true;
 // max execution time
-export const timeout = 30000;
+export const timeout = 60000;
 // to run in 'slow motion' increase this time
 // and to run at maximal speed keep at 0
 export const sleepBetweenSteps = 0;
